7196ab6934e75922c38a5fc346e2d9f929f2060d
Yadira Henao <yadira.henao@udea.edu.co>
2021-09-28 22:11:44 -0500

    Se agrega un nuevo Route para el menú

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Index from 'pages';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import 'styles/styles.css'
+import 'Navbar/Navbar'
+import Navbar from 'Navbar/Navbar';
 
 function App() {
   return (
     <div className='App'>
       <Router>        
           <Switch>
-
+          <Route path='/Menu'>
+              <Navbar/>
+            </Route>
             <Route path='/'>
               <Index />
             </Route>
