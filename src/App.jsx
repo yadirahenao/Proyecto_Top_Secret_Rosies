245374b0369e993f2277d862ed56187be867b505
Yadira Henao <yadira.henao@udea.edu.co>
2021-09-28 20:08:10 -0500

    Modificaciones al App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Index from 'pages';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import 'styles/styles.css'
 
 function App() {
   return (
-    <div className="App">
-      <div class="overlay">
-        <form>
-          <div class="con">
-            <header class="head-form">
-              <h2>Bienvenido</h2>
-              <p>Ingrese su usuario y contraseña</p>
-            </header>
-            <br></br>
-          <div class="field-set">
-            <span class="input-item">
-              <i class="fa fa user-circle"></i>
-            </span>
-            <input class="form-input" id="txt-input" type="text" placeholder="Nombre de usuario" required></input>
-            <br></br>
-            
-            <span class="input-item">
-              <i class="fa fa-key"></i>
-            </span>
-            <input class="form-input" type="password" placeholder="Contraseña" id="pwd"  name="password" required></input>
-
-            <span>
-              <i class="fa fa-eye" aria-hidden="true" type="button" id="eye"></i>
-            </span>
-
-            <br></br>
-            <button class="log-in">Ingresar</button>            
-          </div>
-
-          <div class="other">
-            <button class="btn submits frgt-pass">Has olvidado tu contraseña</button>
-          </div>
-
-          </div>
-        </form>
-      </div>
-    
-    </div>
+    <div className='App'>
+      <Router>        
+          <Switch>
+            <Route path='/inicio'>
+              <MenuInicio/>
+            </Route>
+            <Route path='/'>
+              <Index />
+            </Route>
+          </Switch>        
+      </Router>
+    </div>    
   );
 }
 
