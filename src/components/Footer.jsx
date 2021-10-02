4cfe0cf67a1207a420dad3fd08910abd931c68fd
milecafa <milena.cafa@gmail.com>
2021-10-02 17:08:16 -0500

    Ajustes de diseño a página de inicio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,9 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div className='Footer'>Powered by T.S Rosies</div>;
+  return <div className='Footer'>
+    <h1 classname='t_footer'> Powered by T.S Rosies </h1>
+    </div>
 };
 
 export default Footer;
